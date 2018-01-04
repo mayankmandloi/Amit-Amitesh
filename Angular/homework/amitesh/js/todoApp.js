a8d55291b85730acaddc1d71bc419154ba455637
mayankmandloi <mayankmandloi009@gmail.com>
2018-01-04 10:47:54 +0530

    day-5

---
+++ Angular/homework/amitesh/js/todoApp.js
@@ -0,0 +1,66 @@
+angular.module("todoApp",[]).controller("todoController",function(){
+    var self = this;
+
+    self.sortBy=function(t)
+    {
+        // if(self.sortByMe==t)
+        // {
+        //     self.sortByMe="-"+t; 
+        // }
+        // else
+        // self.sortByMe=t;
+        self.canI =!self.canI;
+        console.log(self.canI)
+    };
+    self.taskList=[{text:"create todo app",done:false}]
+    self.addTask= function()
+    {
+        self.taskList.push({text:self.newTask,done:false})
+        self.newTask="";
+    };
+    self.toggleEdit=function(task)
+    {
+        if(task.editable)
+        {
+            task.editable=false;
+        }
+        else
+        {
+            task.editable=true;
+        }
+    }
+});
+
+angular.module("todoApp").filter("ff",function(){
+    return function(item,canI)
+    {
+        var incTask=[];
+        var cmpTask=[];
+       for(index=0;index<item.length;index++)
+       {
+          // console.log(index);
+           if(item[index].done)
+           {
+               cmpTask.push(item[index]);
+           }
+           else
+           {
+               incTask.push(item[index]);
+           }
+           
+           //console.log(t);
+        }
+        if(canI)
+        {
+        incTask=incTask.sort(function(a,b)
+        {
+            console.log(a.text.localeCompare(b.text));
+            return a.text.localeCompare(b.text);
+        });
+    }
+       // console.log(incTask);
+        var t=incTask.concat(cmpTask);
+        return t;         
+
+    }
+})
